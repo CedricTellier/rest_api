d07b34172f9e2655d2a3c4334cbc19794b81f4ff
Cédric Tellier <cedric.tellier@yahoo.fr>
2021-05-09 14:35:59 +0200

    Correct sequential process tests and remove mangoose operations

--- test/tests.js
+++ test/tests.js
@@ -2,118 +2,59 @@ require('dotenv').config();
 const app = require("../server.js");
 const request = require("supertest");
 const expect = require("chai").expect;
-const getDirs = [ '', '/Caddev', '/CadworkSA', '/Cadcom' , '/Cadskills', '/Capture4cad', '/Cadwork']
-const endpointDir = '/employees';
 const companies = ['Caddev', 'CadworkSA', 'Cadcom' , 'Cadskills', 'Capture4cad', 'Cadwork'];
-const mongoose = require("mongoose");
 
-describe("Test GET", function () {
-getDirs.forEach(directory => {
-  const endpoint = directory + endpointDir;
-  const testDesc = "Test GET " + directory +  " employees"
-    const testName = "GET " + endpoint;
-    step(testName, function (done) {
-      request(app)
-        .get(endpoint)
-        .expect(200)
-        .end(function (err, res) {
-          if (err) done(err);
-          done();
-        });
-    });
-  });
-});
-describe("Test POST employee", function () {
 companies.forEach(company => {
-    step("POST /employees/", function (done) {
-    var concatName = "Test" + company;
-    const newEmployee = {"firstname" : concatName, "lastname" : concatName, "business" : company};
-    request(app)
-        .post("/employees/")
-        .send(newEmployee)
-        .expect('Content-Type', /json/)
-        .end(function (err, res) {
-            if (err) done(err);
-            expect(res)
-                .to.have.nested.property('body')
-                .that.includes.all.keys([ 'id', 'firstname', 'lastname', '_id' , 'created_date'])
-            done();
-        });
-    });
-  });  
-});
-describe("Test PUT employee", function () {
-companies.forEach(company => {
-      var employeesList = [];
-      before(function(done) {
-          mongoose.Promise = global.Promise;
-          mongoose.set('useCreateIndex', true);
-          mongoose.set('useUnifiedTopology', true);
-          mongoose.set('useFindAndModify', false);
-          mongoose.connect(`${process.env.MONGODB_TEST_URI}`, { useNewUrlParser: true }); 
-          Employees = mongoose.model('Employees');
-          Employees.find({ "firstname": { $regex: "Test*" }, "lastname": { $regex: "Test*" }, "business": { $regex: company } }, function(err, employee) {
-              employeesList = employee;
-              done();
-          });
-      });
-      step("PUT /employees/:id", function (done) {
-        var concatName;
-        var modifyEndpoint;
-        var jsonObject = employeesList[0].toObject();
-        for (var key in jsonObject) {
-            if (key == "business") {
-                concatName = "Modified" + jsonObject[key];
-            }
-            else if (key == "_id") {
-                modifyEndpoint = "/employees/" + jsonObject[key];
-            }
-        }
-        const modifyEmployee = {"firstname" : concatName, "lastname" : concatName};
-        request(app)
-            .put(modifyEndpoint)
-            .send(modifyEmployee)
-            .expect(200)
-            .end(function (err, res) {
+    describe("Tests " + company, function () {
+        var id;
+        step("GET " + company, function (done) {
+            request(app)
+              .get("/" + company +  "/employees")
+              .expect(200)
+              .end(function (err, res) {
                 if (err) done(err);
                 done();
-            });
-        });
-    });
-});
-describe("Test DELETE employee", function () {
-companies.forEach(company => {
-      var employeesList = [];
-      before(function(done) {
-          mongoose.Promise = global.Promise;
-          mongoose.set('useCreateIndex', true);
-          mongoose.set('useUnifiedTopology', true);
-          mongoose.set('useFindAndModify', false);
-          mongoose.connect(`${process.env.MONGODB_TEST_URI}`, { useNewUrlParser: true }); 
-          Employees = mongoose.model('Employees');
-          Employees.find({ "firstname": { $regex: "Modified*" }, "lastname": { $regex: "Modified*" }, "business": { $regex: company } }, function(err, employee) {
-              employeesList = employee;
-              done();
+              });
           });
-      });
-      step("DELETE /employees/:id", function (done) {
-        var deleteEndpoint;
-        var jsonObject = employeesList[0].toObject();
-        for (var key in jsonObject) {
-            if (key == "_id") {
-              deleteEndpoint = "/employees/" + jsonObject[key];
-            }
-        }
-        request(app)
-            .delete(deleteEndpoint)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) done(err);
-                expect(res)
-                .to.have.nested.property('body')
-                .that.includes.all.keys([ 'message'])
-                done();
-            });
+        step("POST " + company, function (done) {
+            var concatName = "Test" + company;
+            const newEmployee = {"firstname" : concatName, "lastname" : concatName, "business" : company};
+            request(app)
+                .post("/employees/")
+                .send(newEmployee)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    if (err) done(err);
+                    expect(res)
+                        .to.have.nested.property('body')
+                        .that.includes.all.keys([ 'id', 'firstname', 'lastname', '_id' , 'created_date'])
+                    id = res.body._id;
+                    done();
+                });
         });
+        step("PUT " + company, function (done) {
+            var concatName = "Modified" + company;
+            const modifyEmployee = {"firstname" : concatName, "lastname" : concatName};
+            request(app)
+                .put("/employees/" + id)
+                .send(modifyEmployee)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) done(err);
+                    done();
+                });
+        });
+        step("DELETE " + company, function (done) {
+            request(app)
+                .delete("/employees/" + id)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) done(err);
+                    expect(res)
+                    .to.have.nested.property('body')
+                    .that.includes.all.keys([ 'message'])
+                    done();
+                });
+            });
     });
 });
